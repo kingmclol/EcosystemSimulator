d37eb9af88b80117608206de7e018c6143a8e595
OsmondLin1 <349757484@gapps.yrdsb.ca>
2024-03-29 10:49:30 -0400

    Animal superclass
    Created the  basic template for how the animal superclass will look.

---
+++ Animal.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Animal superclass where subclasses will inherit traits
+ * and behaviours from
+ * 
+ * @author (Osmond Lin) 
+ */
+
+public class Animal extends Actor {
+    private int speed;
+    private int energy;
+
+    public Animal() {
+        
+    }
+
+    public void act() {
+        move(speed);
+        energy--;
+        if (energy <= 0) {
+            die();
+        }
+    }
+
+    public void eat(int energyGain) {
+        energy += energyGain;
+    }
+
+    public void die() {
+        getWorld().removeObject(this);
+    }
+}
+

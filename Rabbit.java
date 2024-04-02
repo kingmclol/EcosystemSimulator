e483fdf023af4a203b88ef27ee63b4c849b9ef60
OsmondLin1 <349757484@gapps.yrdsb.ca>
2024-04-02 11:56:46 -0400

    Added vulture subclass

--- Rabbit.java
+++ Rabbit.java
@@ -24,9 +24,6 @@ public class Rabbit extends Animal
      */
     public void act() {
         super.act();
-        if(timeFlowing == false){
-            return;
-        }
 
         if(alive && !beingEaten){
             if((targetGrass == null) || !(distanceFrom(targetGrass) < 5)){

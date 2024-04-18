8d016ae6bf712cee40c524ef2bec438cd19a0f8d
kingmclol <140989202+kingmclol@users.noreply.github.com>
2024-04-18 11:45:58 -0400

    made introworld cool
    background images exist.

--- IntroWorld.java
+++ IntroWorld.java
@@ -5,6 +5,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * 
  * @author (your name) 
  * @version (a version number or a date)
+ * Images from /u/Voidentir at https://old.reddit.com/r/DigitalArt/comments/1akfavq/my_old_landscape_artworks/
  */
 public class IntroWorld extends CursorWorld
 {
@@ -14,19 +15,61 @@ public class IntroWorld extends CursorWorld
      * 
      */
     BreathingTextBox promptBox;
+    TextBox title;
+    private int actCount;
+    private static String[] backgroundImages = new String[] {
+        "landscape1.png",
+        "landscape2.png",
+        "landscape3.png"
+    };
+    private Picture currentImage;
+    private Picture nextImage;
+    private int index;
     public IntroWorld()
     {
         super();
-        promptBox = new BreathingTextBox("PRESS (L) TO START", 52, Color.BLACK, null, 120);
-        addObject(promptBox, getWidth()/2, getHeight()/2+250);
+        promptBox = new BreathingTextBox("PRESS (L) TO START", 52, Color.RED, null, 120);
+        addObject(promptBox, getWidth()/2, getHeight()/2+300);
+        title = new TextBox("ECOSYSTEM SIMULATOR", 86, Color.BLACK, null, 2, 0);
+        addObject(title, getWidth()/2, 80);
         introWorldMusic = new GreenfootSound("Introworld.mp3");
         introWorldMusic.setVolume(30);
+        actCount = 0;
+        index = 0;
+        currentImage = new Picture(backgroundImages[index], getWidth(), getHeight());
+        addObject(currentImage, getWidth()/2, getHeight()/2);
+        nextImage = new Picture(backgroundImages[nextIndex()], getWidth(), getHeight());
+        nextImage.setTransparency(0);
     }
     public void act() {
-        if ("l".equals(Greenfoot.getKey())) {
+        if (++actCount >= 600) { // every 10 seconds, change background image.
+            switchBackgroundImage();
+            actCount = 0;
+        }
+        if ("l".equals(Greenfoot.getKey())) { // once L is pressed, move to the next world.
             Greenfoot.setWorld(new StoryWorld());
             introWorldMusic.stop(); 
         }
+    }
+    /**
+     * Switches the background image in a cool way.
+     */
+    private void switchBackgroundImage() {
+        currentImage.fadeOut(1); // make the current image begin to disappear.
+        addObject(nextImage,getWidth()/2, getHeight()/2); // add the next image.
+        nextImage.fadeIn(1);
+        // update reference of current image to the next image, as the current image will remove itself fro the world
+        // by itself, so need to keep it anymore.
+        currentImage = nextImage;
+        nextImage = new Picture(backgroundImages[nextIndex()], getWidth(), getHeight()); // have next image prepared.
+        nextImage.setTransparency(0); // set its transparency to 0 so it can fade in.
+    }
+    /**
+     * Returns the next index for the next picture to use.
+     */
+    private int nextIndex() {
+        index = (index + 1)%backgroundImages.length;
+        return index;
     }
     public void started() {
         introWorldMusic.playLoop();

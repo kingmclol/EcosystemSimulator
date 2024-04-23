f62a27a7f8503918a6c86b884059e913a597e5e0
tony1234567891011 <130993859+tony1234567891011@users.noreply.github.com>
2024-04-22 22:59:03 -0400

    comments
    a

--- Moon.java
+++ Moon.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * The indicator that tells the viewer whether it is day or night time.
+ * The indicator that tells the viewer whether if it is night time.
  * 
  * @author Tony Lin
  * @version Mon April 22, 2024
@@ -10,10 +10,10 @@ public class Moon extends SuperActor
 {
     //Instance Variables:
     private GreenfootImage moon;
-    
     /**
-     * Act - do whatever the Moon wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Constructor to initialize instance variables.
+     * Also, set the image of the crescent shape facing right, to fit it being on the
+     * left part of the screen.
      */
     public Moon()
     {
@@ -23,6 +23,10 @@ public class Moon extends SuperActor
         moon.setTransparency(0);
         setImage(moon);
     }
+    /**
+     * Act - do whatever the Moon wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
     public void act()
     {
         // Add your action code here.
--- Sun.java
+++ Sun.java
@@ -1,17 +1,17 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class sun here.
+ * The indicator that tells the viewer whether if it is day time.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Tony Lin 
+ * @version Mon April 22, 2024
  */
 public class Sun extends SuperActor
 {
+    //Instance Variables:
     private GreenfootImage sun;
     /**
-     * Act - do whatever the sun wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Constructor to initialize instance variables.
      */
     public Sun()
     {
@@ -20,41 +20,35 @@ public class Sun extends SuperActor
         sun.setTransparency(180);
         setImage(sun);
     }
+    /**
+     * Act - do whatever the sun wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
     public void act()
     {
         // Add your action code here.
         SimulationWorld world = (SimulationWorld)getWorld();
-        if(world.getNight() == true)
+        if(world.getNight() == true) // if world is nighttime
         {
-            if(getY() < 800)
+            if(getY() < 800) // if not yet at the bottom of the screen
             {
+                //keep moving down
                 this.setLocation(getX(), getY()+(60/12));
-                //figure something out later maybe
-                //int transparencyFactor = (int)(100*Math.pow(0.5, (getX()/200-1)));                   
-                /*if(transparencyFactor < 0)
+                if(this.getY() > 700) // when its close to the bottom of the screen, make it invisible
                 {
-                    sun.setTransparency(0);
-                    setImage(sun);
-                }
-                else
-                {
-                    sun.setTransparency(180*transparencyFactor/100);
-                    setImage(sun);
-                }
-                System.out.println(getX());
-                System.out.println(Math.pow(0.5, (this.getX()/400-1)));*/
-                if(this.getY() > 700)
-                {
-                    sun.setTransparency(0);
+                    sun.setTransparency(0); // transparency to 0
                     setImage(sun);
                 }
                 
             }
         }
-        else
+        else // it should be daytime
         {
-            if(getY() > 150)
+            if(getY() > 150) // if its not near the top of the screen,
             {
+                /*
+                 * move up, make it visible(set its current image transparency val of 180) 
+                 */
                 this.setLocation(getX(), getY()-(50/12));
                 sun.setTransparency(180);
                 setImage(sun);
